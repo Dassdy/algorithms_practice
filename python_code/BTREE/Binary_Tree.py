2ab79c330589dc9b85da9a7fef6bcb605c99179a
Dassdy <1319247245@qq.com>
2021-02-23 18:21:19 +0800

    3

--- python_code/BTREE/Binary_Tree.py
+++ python_code/BTREE/Binary_Tree.py
@@ -5,3 +5,51 @@ from random import sample
 #binary tree class
 
 class BTree(object):
+
+    # initializition
+    def __init__(self, data=None, left=None, right=None):
+        self.data=data
+        self.left=left
+        self.right=right
+        self.dot=Digraph(comment="Binary Tree")
+
+    # preorder traverse
+    def preorder(self):
+        if self.data:
+            print(self.data, end=' ')
+        if self.left:
+            self.left.preorder()
+        if self.right:
+            self.right.preorder()
+    
+    # inorder traverse
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
+        if self.data:
+            print(self.data, end=' ')
+        if self.right:
+            self.right.inorder()
+
+    # postorder traverse
+    def postorder(self):
+        if self.left:
+            self.left.postorder()
+        if self.right:
+            self.right.postorder()
+        if self.data:
+            print(self.data, end=' ')
+        
+    # levelorder traverse
+    def levelorder(self):
+
+    # height of binary tree
+    def height(self):
+
+    # leaves of binary tree
+    def leaves(self):
+
+    # show the binary tree
+    def print_tree(self, save_path='./Binary_Tree.gv', label=False):
+
+        
