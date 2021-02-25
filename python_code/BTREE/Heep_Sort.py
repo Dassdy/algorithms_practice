2fdc883b1133ff7ce6d04e5afa4966c0f4f71f10
Dassdy <1319247245@qq.com>
2021-02-26 00:47:03 +0800

    5

--- python_code/BTREE/Heep_Sort.py
+++ python_code/BTREE/Heep_Sort.py
@@ -10,12 +10,12 @@ def heepAdjust(arrayToSort, start):
             if arrayToSort[i] < arrayToSort[2*i+1]:
                 arrayToSort[i]=arrayToSort[2*i+1]
                 arrayToSort[2*i+1]=temp
-                i=2*i+1
+            i=2*i+1
         else:
             if arrayToSort[i] < arrayToSort[2*i]:
                 arrayToSort[i]=arrayToSort[2*i]
                 arrayToSort[2*i]=temp
-                i=2*i
+            i=2*i
     if 2*i==len(arrayToSort)-1:
         temp = arrayToSort[i]
         if arrayToSort[i] < arrayToSort[2*i]:
@@ -23,24 +23,24 @@ def heepAdjust(arrayToSort, start):
             arrayToSort[2*i]=temp
 
 def heepSort(arrayToSort):
-
+    arraySorted=[]
     if len(arrayToSort)<2:
         pass
     else:
-        arrayToSort.append(0)
+        arrayToSort.append(70)
         for i in range(0, int((len(arrayToSort)-1)/2)):
             heepAdjust(arrayToSort, int((len(arrayToSort)-1)/2-i))
-        lengthOfInit=len(arrayToSort)
-        for i in range(1, len(arrayToSort)):
-            swapNodes(arrayToSort[i],arrayToSort[-1])
-            heepAdjust(arrayToSort[0,lengthOfInit-i], int((lengthOfInit-1)/2-i))
-            
 
-    return arrayToSort
+        for i in range(1, len(arrayToSort)):
+            arrayToSort[1], arrayToSort[-1]=arrayToSort[-1], arrayToSort[1]
+            arraySorted.append(arrayToSort[-1])
+            arrayToSort.remove(arrayToSort[-1])
+            heepAdjust(arrayToSort, 1)
+    return arraySorted
 
 
 def main():
-    L = [50, 16, 30, 10, 60,  90,  2, 80, 70]
+    L = [0,50, 16, 30, 10, 60,  90,  2, 80,1,4,3,9,5,-1]
     print(heepSort(L))
 
 
